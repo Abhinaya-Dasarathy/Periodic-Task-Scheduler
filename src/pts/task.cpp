0e26a159f67b6653746d7ba28dcbfc3f9bad1bc8
Abhinaya-Dasarathy <abhi20das@gmail.com>
2017-05-31 17:13:35 -0700

    Update task.cpp

--- src/pts/task.cpp
+++ src/pts/task.cpp
@@ -1,3 +1,5 @@
+
+
 /*
  * task.cpp
  *
@@ -5,6 +7,54 @@
  *      Author: abhi2
  */
 
+#include "task.hpp"
+#include <chrono>
+#include <ctime>
+
+using namespace pts::task;
+
+TaskFactory& TaskFactory::Get() {
+    static TaskFactory factory;
+    return factory;
+}
+
+void TaskFactory::Register(TaskRegistrarInterface *registrar,
+        std::string name, unsigned int sec) {
+    registry[name] = std::make_pair(registrar, sec);
+}
+
+TaskInterface* TaskFactory::GetTask(std::string name) {
+    TaskRegistrarInterface* registrar;
+    auto entry = registry.find(name);
+    if (entry == registry.end()) {
+        throw std::out_of_range("Task Not Foud");
+    }
+    return entry->second.first->GetTask();
+}
+
+unsigned int TaskFactory::GetTaskInterval(std::string name) {
+    TaskRegistrarInterface* registrar;
+    auto entry = registry.find(name);
+    if (entry == registry.end()) {
+        throw std::out_of_range("Task Not Foud");
+    }
+    return entry->second.second;
+}
+
+std::vector<std::string> TaskFactory::GetTaskNames() {
+    std::vector<std::string> vs;
+    for (auto &mod : registry) {
+        vs.push_back(mod.first);
+    }
+    return vs;
+}
+
+void PersistTask::persist(float& data) {
+    if (!db->do_query("INSERT INTO " + store + " VALUES (" +
+                std::to_string(data) + "," + "datetime('now')" + ")")) {
+        throw "failed to insert in store";
+    }
+}
 
 
 
